4939e086a52a13b273019050fbe4b01d9c89f734
Lasse Lybeck <lybeck.lasse@gmail.com>
2016-01-14 17:25:10 +0200

    Added camera and ball scripts that do not work.

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -1,7 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class BallMovement : MonoBehaviour {
+
+	public float rotationSpeed = .1f;
+	public float movementSpeed = 5f;
+	public float jumpSpeed = 15f;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +14,11 @@ public class BallMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Rigidbody rigidbody = GetComponent<Rigidbody> ();
+		float direction = Input.GetAxis("Horizontal");
+		float acceleration = Input.GetAxis("Vertical");
+		float jumpAcceleration = Input.GetButton ("Jump") ? 1f : 0f;
+		rigidbody.AddForce (new Vector3 (0, jumpSpeed * jumpAcceleration, movementSpeed * acceleration));
+		transform.Rotate (Vector3.up, direction * rotationSpeed);
 	}
 }
---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public GameObject ball;
+	public Vector3 cameraOffset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Rigidbody rigidBody = ball.GetComponent<Rigidbody> ();
+		Vector3 velocity = rigidBody.velocity;
+		velocity.y = 0;
+		Vector3 offset = new Vector3 ();
+		offset = velocity.normalized * cameraOffset.z;
+		offset.y = cameraOffset.y;
+		transform.position = ball.transform.position + offset;
+
+		transform.rotation = Quaternion.Euler (new Vector3(30, Vector3.Angle (Vector3.forward, velocity), 0));
+	}
+}
