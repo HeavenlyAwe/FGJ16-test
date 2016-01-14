701d4a6a11cf5b9a54fe7e177a4b90ca63c4740d
Christoffer Fridlund <christoffer.fridlund@hotmail.com>
2016-01-14 18:22:55 +0200

    Fixed movement scripts.

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -1,24 +1,27 @@
 using UnityEngine;
 using System.Collections;
 
-public class BallMovement : MonoBehaviour {
+public class BallMovement : MonoBehaviour
+{
 
-	public float rotationSpeed = .1f;
-	public float movementSpeed = 5f;
-	public float jumpSpeed = 15f;
+    public float rotationSpeed = .1f;
+    public float movementSpeed = 100f;
+    public float jumpSpeed = 15f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Rigidbody rigidbody = GetComponent<Rigidbody> ();
-		float direction = Input.GetAxis("Horizontal");
-		float acceleration = Input.GetAxis("Vertical");
-		float jumpAcceleration = Input.GetButton ("Jump") ? 1f : 0f;
-		rigidbody.AddForce (new Vector3 (0, jumpSpeed * jumpAcceleration, movementSpeed * acceleration));
-		transform.Rotate (Vector3.up, direction * rotationSpeed);
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        float accelerationZ = Input.GetAxis("Vertical");
+        float accelerationX = Input.GetAxis("Horizontal");
+
+        Vector3 direction = Camera.main.transform.TransformDirection(new Vector3(accelerationX, 0f, accelerationZ));
+        rigidbody.AddForce(direction * movementSpeed);
+    }
 }
--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -13,14 +13,13 @@ public class CameraMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Rigidbody rigidBody = ball.GetComponent<Rigidbody> ();
-		Vector3 velocity = rigidBody.velocity;
-		velocity.y = 0;
-		Vector3 offset = new Vector3 ();
-		offset = velocity.normalized * cameraOffset.z;
-		offset.y = cameraOffset.y;
-		transform.position = ball.transform.position + offset;
 
-		transform.rotation = Quaternion.Euler (new Vector3(30, Vector3.Angle (Vector3.forward, velocity), 0));
-	}
+        Vector3 forward = transform.forward.normalized;
+        Vector3 offset = new Vector3(forward.x, 0f, forward.z) * cameraOffset.z;
+        offset.y = cameraOffset.y;
+        transform.position = ball.transform.position + offset;
+
+        float mouseDx = Input.GetAxis("Mouse X");
+        transform.RotateAround(ball.transform.position, Vector3.up, mouseDx);
+    }
 }
